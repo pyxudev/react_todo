e31c8b0eee6df9b4eca882df92255594fe295f71
pyxudev <xsq51py@gmail.com>
2022-03-20 06:57:12 +0900

    first commit

--- src/index.js
+++ src/index.js
@@ -1,13 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+//import App from './App';
+import Todo from './todo';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Todo />,
   document.getElementById('root')
 );
 
